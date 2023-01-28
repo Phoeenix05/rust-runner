693266aeb0f90b739d79e659187f050ae2545aea
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-01-28 22:14:10 +0200

    Added `search` and `run` commands

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,26 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[clap(author, version)]
-pub struct Args {
+#[clap(author = "Anton Fredriksson", version, about)]
+pub struct Arguments {
+    #[command(subcommand)]
+    pub commands: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Search(SearchCommand),
+    Run(RunCommand),
+}
+
+#[derive(Debug, Args)]
+pub struct SearchCommand {
+    pub target: String,
+}
+
+#[derive(Debug, Args)]
+pub struct RunCommand {
+    /// build target for [rust]
     #[arg(short, long)]
     pub target: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,28 @@
 use clap::Parser;
-use std::process::{Command, Stdio};
+// use std::process::{Command, Stdio};
 
 mod args;
 
-/// if the user has specified a target, then print the list of installed targets that match the user's
-/// target
 fn main() {
-    let args = args::Args::parse();
+    let args = args::Arguments::parse();
+    println!("{:?}", args);
 
-    if args.target.is_some() {
-        // let target = args.target.unwrap();
+    // if args.target.is_some() {
+    //     let target_list = Command::new("rustup")
+    //         // .args(["target", "list"])
+    //         .arg("target")
+    //         .arg("list")
+    //         .stdout(Stdio::piped())
+    //         .spawn()
+    //         .expect("Failed to execute 'rustup' command");
 
-        let target_list = Command::new("rustup")
-            // .args(["target", "list"])
-            .arg("target")
-            .arg("list")
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("Failed to execute 'rustup' command");
+    //     let arg = format!("/{}/ && /installed/ {{print}}", args.target.unwrap());
+    //     let targets = Command::new("awk")
+    //         .arg(arg)
+    //         .stdin(target_list.stdout.unwrap())
+    //         .output()
+    //         .expect("Failed to execute 'awk' command");
 
-        let arg = format!("/{}/ && /installed/ {{print}}", args.target.unwrap());
-        let targets = Command::new("awk")
-            .arg(arg)
-            .stdin(target_list.stdout.unwrap())
-            .output()
-            .expect("Failed to execute 'awk' command");
-
-        print!("{}", String::from_utf8_lossy(&targets.stdout))
-
-        // let installed = Command::new("grep")
-        //     .arg("installed")
-        //     .stdin(targets.stdout.unwrap())
-        //     .output()
-        //     .expect("Failed to execute 'grep' command");
-
-        // println!("{}", String::from_utf8_lossy(&installed.stdout))
-    }
+    //     print!("{}", String::from_utf8_lossy(&targets.stdout))
+    // }
 }
